3a3f444b6fb465d3ebf696f056f53769a99fe7af
Bekarys Amanbay <bekarys.amanbai@gmail.com>
2024-11-26 04:13:34 +0500

    settings router

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({}),
     vue(),
     vueDevTools(),
     AutoImport({
